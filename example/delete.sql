074a35de9d38f7ee511dfb19b8a4e642aca6172d
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2021-11-19 15:40:20 +0000

    SQL Examples

---
+++ example/delete.sql
@@ -0,0 +1,4 @@
+-- DELETE FROM tbl_table;
+
+DELETE FROM llamadas WHERE contacts_id = 7;
+DELETE FROM contacts WHERE id = 7;
--- example/select.sql
+++ example/select.sql
@@ -8,3 +8,9 @@ SELECT name, phone FROM contacts WHERE name = 'Antonio';
 SELECT id, name, phone FROM contacts WHERE id in (1, 3, 5);
 
 SELECT name, phone, email FROM contacts WHERE email IS NULL;
+
+
+-- JOINS
+
+SELECT c.name, comment, fecha FROM llamadas AS l
+RIGHT JOIN contacts AS c ON l.contacts_id = c.id;
